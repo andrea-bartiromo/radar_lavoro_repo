5f0bdab615396169f76c7733dc56266aecd3e468
andrea-bartiromo <bart3052@gmail.com>
2026-07-02 13:23:38 +0200

    Crea package services

---
+++ services/__init__.py
@@ -0,0 +1,5 @@
+"""Servizi applicativi di Radar Lavoro.
+
+Questo package conterrà progressivamente la logica oggi presente in app.py:
+profilo, ranking, filtri, fonti dati, deduplica e normalizzazione.
+"""
